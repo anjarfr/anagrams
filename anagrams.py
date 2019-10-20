de0c57f6d749cedb443d74ced07bc88e31936b18
Anja Rosvold From <anjarfrom@gmail.com>
2019-10-20 14:32:25 +0200

    Read word directly from file to avoid duplication of data

--- anagrams.py
+++ anagrams.py
@@ -3,40 +3,27 @@ from typing import List
 import os
 
 
-def read_file(filename: str) -> List[str]:
+def find_anagrams(filename: str) -> dict:
     """
-    Opens a text file and returns all the words in the
-    file as a list
+    Opens a text file and finds anagrams by sorting each
+    word alphabetically. Appends each word to the list of
+    the dictionary key that corresponds to the sorted word
 
     :param filename:str: name of the file being opened. Path is relative to current directory
-    :raises: 
-    :rtype: 
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        words = file.read()
-        word_array = words.split('\n')
-    return word_array
-
-
-def find_anagrams(word_list: List[str]) -> dict:
-    """ 
-    Finds anagrams by sorting each word alphabetically
-    Appends each word to the list of the dictionary key
-    that corresponds to the sorted word
-
-    :param word_list:List[str]: list of words
-    :raises: 
+    :raises: FileNotFoundError if filename doesn't resolve to a relative path
     :rtype: dictionary of type {key=sorted word: value=[words]}
     """
     anagrams = defaultdict(list)
-    for word in word_list:
-        anagrams[''.join(sorted(word))].append(word)
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            word = line.strip('\n')
+            anagrams[''.join(sorted(word))].append(word)
     return anagrams
 
 
 def print_anagrams(anagrams: dict):
     """ 
-    Prints all anagrams found in the list of words
+    Prints all anagrams in the file
 
     :param anagrams:dict: Dictionary containing all possible anagrams
     :raises:
@@ -47,11 +34,14 @@ def print_anagrams(anagrams: dict):
             print(', '.join(word for word in anagram))
 
 
-def main():
-    words = read_file('eventyr.txt')
-    anagrams = find_anagrams(words)
-    print_anagrams(anagrams)
+def main(filename: str):
+    try:
+        anagrams = find_anagrams(filename)
+        print_anagrams(anagrams)
+    except OSError as e:
+        print('Could not find the file ', filename)
+        print(e)
 
 
 if __name__ == '__main__':
-    main()
+    main('eventyr.txt')
